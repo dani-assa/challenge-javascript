9a8979ef917ff09e181392a2287b4f70422ec114
Dani Assa <dfassa1@gmail.com>
2023-10-18 20:49:33 -0300

    generando cards

--- js/app.js
+++ js/app.js
@@ -3,60 +3,60 @@ Nombre, Apellido, Fecha de nacimiento, email, género y foto (link a una imagen 
 
 Al presionar un botón (para enviar los datos) dicha información deberá guardarse en localStorage como un objeto y al mismo tiempo deberá mostrar en pantalla una card con toda la información del usuario (incluyendo su foto)*/
 
+let arrayUser = JSON.parse(localStorage.getItem('user')) || [];
 
-class Persona {
-  constructor (nombre, apellido, FechaDeNacimiento, email, genero, foto) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.FechaDeNacimiento = FechaDeNacimiento;
-    this.email = email;
-    this.genero = genero;
-    this.foto = foto
-  }
-};
 
-let nombre = document.querySelector('#inputNombre');
-let apellido = document.querySelector('#inputApellido');
-let fechaDeNacimiento = document.querySelector('#inputFechaDeNac');
-let email = document.querySelector('#inputEmail');
-let genero = document.querySelector('#inputGenero');
-let foto = document.querySelector('#inputFoto');
 
 const formulario = (e) => {
   e.preventDefault();
-  const nuevaPersona = new Persona(
-    nombre.value,
-    apellido.value,
-    fechaDeNacimiento.value,
-    email.value,
-    genero.value,
-    foto.value
-  );
 
+  const nombre = document.querySelector('#inputNombre').value;
+  const apellido = document.querySelector('#inputApellido').value;
+  const fechaDeNacimiento = document.querySelector('#inputFechaDeNac').value;
+  const email = document.querySelector('#inputEmail').value;
+  const genero = document.querySelector('#inputGenero').value;
+  const foto = document.querySelector('#inputFoto').value;
+  
+  const user = {
+    nombre,
+    apellido,
+    fechaDeNacimiento,
+    email,
+    genero,
+    foto
+  };
 
-const card = document.querySelector('#card');
-card.className = "container"
-card.innerHTML = `
-<div class="card mt-3" style="width: 18rem;">
-  <img src="${foto.value}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${nombre.value} ${apellido.value}</h5>
-    <p class="card-text">${email.value}</p>
-    <p class="card-text">${genero.value}</p>
-    <p class="card-text">${fechaDeNacimiento.value}</p>
-  </div>
-</div>
-`
-const arrayDatos = JSON.parse(localStorage.getItem('user')) || [];
-arrayDatos.push(nuevaPersona);
-const personaJson = JSON.stringify(arrayDatos);
-localStorage.setItem('user', personaJson);
-const personaObjeto = JSON.parse(personaJson);
-console.log(personaObjeto);
+  arrayUser.push(user);
 
+  const personaJson = JSON.stringify(arrayUser);
+  localStorage.setItem('user', personaJson);
+
+  const printCard = () => {
+    const cards = document.getElementById('card');
+    cards.classList = 'container';
+    cards.innerHTML = '';
+    arrayUser.map((usuario, i) => {
+      const card = document.createElement('div');
+      card.className = 'user-card'
+      card.innerHTML = `
+      <div class="card mt-3" style="width: 18rem;">
+        <img src="${usuario.foto}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${usuario.nombre} ${usuario.apellido}</h5>
+          <p class="card-text">${usuario.email}</p>
+          <p class="card-text">${usuario.genero}</p>
+          <p class="card-text">${usuario.fechaDeNacimiento}</p>
+        </div>
+      </div>
+      `;
+      cards.appendChild(card);
+    })
+  };
+  printCard()
 };
 
 
 
-document.querySelector('#formDatos').addEventListener('submit', formulario);
+
+document.getElementById('formDatos').addEventListener('submit', formulario);
 
