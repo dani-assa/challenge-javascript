337a1ab1c7182d1d1ed38b4f6a50cc959f3920b9
Dani Assa <dfassa1@gmail.com>
2023-10-19 11:27:50 -0300

    modificando cards

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@ Nombre, Apellido, Fecha de nacimiento, email, género y foto (link a una imagen 
 
 Al presionar un botón (para enviar los datos) dicha información deberá guardarse en localStorage como un objeto y al mismo tiempo deberá mostrar en pantalla una card con toda la información del usuario (incluyendo su foto)*/
 
+
+
 let arrayUser = JSON.parse(localStorage.getItem('user')) || [];
-
-
 
 const formulario = (e) => {
   e.preventDefault();
@@ -25,38 +25,50 @@ const formulario = (e) => {
     genero,
     foto
   };
-
+  
+  
   arrayUser.push(user);
 
   const personaJson = JSON.stringify(arrayUser);
   localStorage.setItem('user', personaJson);
 
-  const printCard = () => {
-    const cards = document.getElementById('card');
-    cards.classList = 'container';
-    cards.innerHTML = '';
-    arrayUser.map((usuario, i) => {
-      const card = document.createElement('div');
-      card.className = 'user-card'
-      card.innerHTML = `
-      <div class="card mt-3" style="width: 18rem;">
-        <img src="${usuario.foto}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${usuario.nombre} ${usuario.apellido}</h5>
-          <p class="card-text">${usuario.email}</p>
-          <p class="card-text">${usuario.genero}</p>
-          <p class="card-text">${usuario.fechaDeNacimiento}</p>
-        </div>
-      </div>
-      `;
-      cards.appendChild(card);
-    })
-  };
+ 
+  limpiarForm()
   printCard()
 };
 
+function limpiarForm() {
+  document.querySelector('#inputNombre').value = '';
+  document.querySelector('#inputApellido').value = '';
+  document.querySelector('#inputFechaDeNac').value = '';
+  document.querySelector('#inputEmail').value = '';
+  document.querySelector('#inputGenero').value = '';
+  document.querySelector('#inputFoto').value = '';
+};
+
+function printCard() {
+  const cards = document.getElementById('card');
+  cards.classList = 'container d-flex justify-content-evenly';
+  cards.innerHTML = '';
+  arrayUser.map((usuario, i) => {
+    const card = document.createElement('div');
+    card.className = 'card mt-3'
+    card.style = 'width: 18rem;'
+    card.innerHTML = `
+      <img src="${usuario.foto}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${usuario.nombre} ${usuario.apellido}</h5>
+        <p class="card-text">Email: ${usuario.email}</p>
+        <p class="card-text">Genero: ${usuario.genero}</p>
+        <p class="card-text">Nacimiento: ${usuario.fechaDeNacimiento}</p>
+      </div>
+    `;
+    cards.appendChild(card);
+  })
+};
 
 
 
 document.getElementById('formDatos').addEventListener('submit', formulario);
 
+printCard()
